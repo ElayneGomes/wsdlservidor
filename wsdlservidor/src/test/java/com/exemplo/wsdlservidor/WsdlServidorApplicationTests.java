d81e5f4c34a6079bebbed959923cd632e046749d
Elayne Gomes <elaynegomes@gmail.com>
2021-02-13 15:27:58 -0300

    Iniciando o projeto wsdl-servidor

---
+++ wsdlservidor/src/test/java/com/exemplo/wsdlservidor/WsdlServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.wsdlservidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdlServidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
